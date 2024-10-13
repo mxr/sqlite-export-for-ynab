d9be90ba54876bd9e86aca58705dd79edf11da7d
Max R <mxr@users.noreply.github.com>
2024-10-12 21:58:14 -0400

    tests

--- testing/fixtures.py
+++ testing/fixtures.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import sqlite3
+from typing import Any
 from uuid import uuid4
 
 import pytest
@@ -10,7 +11,7 @@ from sqlite_export_for_ynab._main import contents
 BUDGET_ID_1 = str(uuid4())
 BUDGET_ID_2 = str(uuid4())
 
-BUDGETS = [
+BUDGETS: list[dict[str, Any]] = [
     {
         "id": BUDGET_ID_1,
         "name": "Budget 1",
@@ -26,6 +27,32 @@ LKOS = {
     BUDGET_ID_2: 107668,
 }
 
+ACCOUNT_ID_1 = str(uuid4())
+ACCOUNT_ID_2 = str(uuid4())
+
+ACCOUNTS: list[dict[str, Any]] = [
+    {
+        "id": ACCOUNT_ID_1,
+        "name": "Account 1",
+        "type": "checking",
+        "debt_escrow_amounts": {
+            "2024-01-01": 160000,
+        },
+        "debt_interest_rates": {
+            "2024-02-01": 5000,
+        },
+        "debt_minimum_payments": {},
+    },
+    {
+        "id": ACCOUNT_ID_2,
+        "name": "Account 2",
+        "type": "savings",
+        "debt_escrow_amounts": {},
+        "debt_interest_rates": {},
+        "debt_minimum_payments": {},
+    },
+]
+
 
 @pytest.fixture
 def cur():
@@ -36,3 +63,7 @@ def cur():
             zip([name for name, *_ in c.description], row, strict=True)
         )
         yield cursor
+
+
+def strip_nones(d: dict[str, Any]) -> dict[str, Any]:
+    return {k: v for k, v in d.items() if v is not None}
--- tests/_main_test.py
+++ tests/_main_test.py
@@ -6,10 +6,17 @@ import pytest
 
 from sqlite_export_for_ynab import default_db_path
 from sqlite_export_for_ynab._main import get_last_knowledge_of_server
+from sqlite_export_for_ynab._main import insert_accounts
 from sqlite_export_for_ynab._main import insert_budgets
+from testing.fixtures import ACCOUNT_ID_1
+from testing.fixtures import ACCOUNT_ID_2
+from testing.fixtures import ACCOUNTS
+from testing.fixtures import BUDGET_ID_1
+from testing.fixtures import BUDGET_ID_2
 from testing.fixtures import BUDGETS
 from testing.fixtures import cur
 from testing.fixtures import LKOS
+from testing.fixtures import strip_nones
 
 
 @pytest.mark.parametrize(
@@ -28,3 +35,58 @@ def test_default_db_path(monkeypatch, xdg_data_home, expected_prefix):
 def test_get_last_knowledge_of_server(cur):
     insert_budgets(cur, BUDGETS, LKOS)
     assert get_last_knowledge_of_server(cur) == LKOS
+
+
+@pytest.mark.usefixtures(cur.__name__)
+def test_insert_budgets(cur):
+    insert_budgets(cur, BUDGETS, LKOS)
+    cur.execute("SELECT * FROM budgets ORDER BY name")
+    assert cur.fetchall() == [
+        {
+            "id": BUDGET_ID_1,
+            "name": BUDGETS[0]["name"],
+            "last_knowledge_of_server": LKOS[BUDGET_ID_1],
+        },
+        {
+            "id": BUDGET_ID_2,
+            "name": BUDGETS[1]["name"],
+            "last_knowledge_of_server": LKOS[BUDGET_ID_2],
+        },
+    ]
+
+
+@pytest.mark.usefixtures(cur.__name__)
+def test_insert_accounts(cur):
+    insert_accounts(cur, BUDGET_ID_1, ACCOUNTS)
+    cur.execute("SELECT * FROM accounts ORDER BY name")
+    assert [strip_nones(d) for d in cur.fetchall()] == [
+        {
+            "id": ACCOUNT_ID_1,
+            "name": ACCOUNTS[0]["name"],
+            "TYPE": ACCOUNTS[0]["type"],
+            "budget_id": BUDGET_ID_1,
+        },
+        {
+            "id": ACCOUNT_ID_2,
+            "name": ACCOUNTS[1]["name"],
+            "TYPE": ACCOUNTS[1]["type"],
+            "budget_id": BUDGET_ID_1,
+        },
+    ]
+    cur.execute("SELECT * FROM account_periodic_values ORDER BY name")
+    assert cur.fetchall() == [
+        {
+            "account_id": ACCOUNT_ID_1,
+            "budget_id": BUDGET_ID_1,
+            "name": "debt_escrow_amounts",
+            "DATE": "2024-01-01",
+            "amount": 160000,
+        },
+        {
+            "account_id": ACCOUNT_ID_1,
+            "budget_id": BUDGET_ID_1,
+            "name": "debt_interest_rates",
+            "DATE": "2024-02-01",
+            "amount": 5000,
+        },
+    ]
