7d508e0d1f87bfcd7d1a2452eb43557b04452571
Max R <mxr@users.noreply.github.com>
2025-06-14 17:58:21 -0400

    Fix category ID and name for subtransactions (#106)

--- sqlite_export_for_ynab/ddl/create-relations.sql
+++ sqlite_export_for_ynab/ddl/create-relations.sql
@@ -161,16 +161,10 @@ SELECT
     , COALESCE(st.id, t.id) AS id
     , COALESCE(st.amount, t.amount) AS amount
     , COALESCE(st.amount, t.amount) / -1000.0 AS amount_major
-    , CASE
-        WHEN
-            COALESCE(st.transfer_account_id, t.transfer_account_id) IS NULL
-            THEN COALESCE(st.category_id, t.category_id)
-    END AS category_id
-    , CASE
-        WHEN
-            COALESCE(st.transfer_account_id, t.transfer_account_id) IS NULL
-            THEN COALESCE(st.category_name, t.category_name)
-    END AS category_name
+    , CASE WHEN st.id IS NULL THEN t.category_id ELSE st.category_id END
+        AS category_id
+    , CASE WHEN st.id IS NULL THEN t.category_name ELSE st.category_name END
+        AS category_name
     , COALESCE(NULLIF(st.memo, ''), NULLIF(t.memo, '')) AS memo
     , COALESCE(st.payee_id, t.payee_id) AS payee_id
     , COALESCE(st.payee_name, t.payee_name) AS payee_name
@@ -251,16 +245,10 @@ SELECT
     , COALESCE(st.id, t.id) AS id
     , COALESCE(st.amount, t.amount) AS amount
     , COALESCE(st.amount, t.amount) / -1000.0 AS amount_major
-    , CASE
-        WHEN
-            COALESCE(st.transfer_account_id, t.transfer_account_id) IS NULL
-            THEN COALESCE(st.category_id, t.category_id)
-    END AS category_id
-    , CASE
-        WHEN
-            COALESCE(st.transfer_account_id, t.transfer_account_id) IS NULL
-            THEN COALESCE(st.category_name, t.category_name)
-    END AS category_name
+    , CASE WHEN st.id IS NULL THEN t.category_id ELSE st.category_id END
+        AS category_id
+    , CASE WHEN st.id IS NULL THEN t.category_name ELSE st.category_name END
+        AS category_name
     , COALESCE(NULLIF(st.memo, ''), NULLIF(t.memo, '')) AS memo
     , COALESCE(st.payee_id, t.payee_id) AS payee_id
     , COALESCE(st.transfer_account_id, t.transfer_account_id)
