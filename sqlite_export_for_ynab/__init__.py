b19d01648162aa20bc31277a0ff69497a0eec34b
Max R <mxr@users.noreply.github.com>
2026-02-08 21:24:16 -0500

    Update __all__ declaration in __init__.py

--- sqlite_export_for_ynab/__init__.py
+++ sqlite_export_for_ynab/__init__.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 from sqlite_export_for_ynab._main import default_db_path
 from sqlite_export_for_ynab._main import sync
 
-__all__ = [default_db_path.__name__, sync.__name__]
+__all__ = ['default_db_path', 'sync']
